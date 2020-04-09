30061b5097670b53b25735c61514020d9b4615c1
Nazar Gargol <nazargargol@gmail.com>
2020-04-09 17:17:29 +1200

    Cleanup of redundant and old code

--- app/components/modal-billing.js
+++ app/components/modal-billing.js
@@ -1,15 +1,4 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
 
 export default ModalComponent.extend({
-    init() {
-        this._super(...arguments);
-    },
-
-    didInsertElement() {
-        this._super(...arguments);
-    },
-
-    willDestroyElement() {
-        this._super(...arguments);
-    }
 });
